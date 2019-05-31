0c9d3f19ec1657e51dbdf71e58728f262d1872ad
Miriam <miriamu88@gmail.com>
2019-05-30 22:03:49 -0500

    starting hooks, issues with material UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import { Typography } from '@material-ui/core';
 
-function App() {
+const App = () => {
+  const [ todos, addTodo, deleteTodo ] = useState([]);
   return (
-    <div>
-      To do List
-    </div>
+    <div className="App">
+      <Typography component="h1" variant="h2">To Do List</Typography>
+      <TodoForm 
+        // saveTodo={console.warn}
+        saveTodo= {todoText => {
+          const trimmedText = todoText.trim();
+            
+          if (trimmedText.length >0) {
+            addTodo(trimmedText);
+          }}
+           }
+          />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+
+      </div>
   );
-}
+};
 
 export default App;
+
+// const rootElement = document.getElementById('root');
+// ReactDOM.render(<App />, rootElement);
